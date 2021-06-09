a5a5efd0d3745285f16f6ce35deb33f365a5cb7a
paul.bandi <paul.bandi@emanuel.ro>
2021-06-09 19:32:18 +0300

    Adding files

---
+++ WebApplicationMovies/Data/WebApplicationMoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MoviesWebApplication.Models;
+
+namespace MoviesWebApplication.Data
+{
+    public class MoviesWebApplicationContext : DbContext
+    {
+        public MoviesWebApplicationContext(DbContextOptions<MoviesWebApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MoviesWebApplication.Models.Movie> Movie { get; set; }
+    }
+}
